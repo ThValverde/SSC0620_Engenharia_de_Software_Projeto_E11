8821b12f8c80f87eb184d19ce5865aaf6ee37b80
Thiago de Castro Valverde <thiago.valverde@wr.sc.usp.br>
2026-06-23 09:10:12 -0300

    Testando alterações e correção de erro

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -148,10 +148,13 @@ REST_FRAMEWORK = {
     ],
 }
 
-REST_USE_JWT = True
-JWT_AUTH_COOKIE = 'jwt-auth'
-JWT_AUTH_REFRESH_COOKIE = 'jwt-refresh'
-JWT_AUTH_HTTPONLY = False
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_COOKIE': 'jwt-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'jwt-refresh',
+    'JWT_AUTH_HTTPONLY': False,
+    'USER_DETAILS_SERIALIZER': 'inventario.serializers.CustomUserDetailsSerializer',
+}
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # =============================================================================
